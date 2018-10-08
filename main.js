0738c1fbb51227faa405909f1aa08467f56edb7e
Smeet Patel <patelsmee@myvuw.ac.ac>
2018-10-08 20:46:41 +1300

    web
    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+for (let i =0; i < 5; i++) {
+console.log("Hello")
+console.log('World');
+}
+function greet ("name" = name);
+
+let myObject ={
+	X:5,
+	y: 10,
+	product: function() {
+		return this.x * this
+	},
+};
+
+let myfunlong = function(x,y){
+	return x * y;
+}
+
+let myfunc = (x, y) => x * y
+
+alert(myfunc(10,17));
+}
