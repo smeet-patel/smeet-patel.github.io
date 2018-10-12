77ad88bd1d36aa92f3b731d822c7317d831c0a5c
Smeet Patel <patelsmee@myvuw.ac.ac>
2018-10-12 18:49:39 +1300

    Adding Icon,making a js file
    Cleaning up index

---
+++ Smeetjavascript.js
@@ -0,0 +1,10 @@
+// . The Magic Code using Javascript which allows the meun button
+// to appear and enabling the nav bar to be responsive
+function myFunction() {
+    var x = document.getElementById("mynav1");
+    if (x.className === "nav1") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav1";
+    }
+}
