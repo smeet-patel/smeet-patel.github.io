38c9a0bdd294b670e81b61db9a74908b9564167d
Smeet <smeetprpatel@gmail.com>
2018-11-22 22:04:56 +1300

    progress bar fixed

--- Smeetjavascript.js
+++ Smeetjavascript.js
@@ -8,10 +8,9 @@ function myFunction() {
         x.className = "nav1";
     }
 }
-//
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction1()};
 
-function myFunction() {
+function myFunction1() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
